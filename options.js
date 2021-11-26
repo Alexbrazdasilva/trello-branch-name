7c942995e5e9772f1b2f8509dddba993451d6e9a
Alexbrazdasilva <alexbrazdasilva@gmail.com>
2021-11-25 23:28:39 -0300

    🐛 Correção no diretório

--- options.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-  options.onlyCopyHash.checked = JSON.parse(localStorage.onlyCopyHash);
-
-  options.onlyCopyHash.onchange = function() {
-    localStorage.onlyCopyHash = options.onlyCopyHash.checked;
-  };
-});
