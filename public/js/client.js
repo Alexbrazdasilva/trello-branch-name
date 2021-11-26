609a6f427550c01b31aae35b514e29a655b9d333
Alexbrazdasilva <alexbrazdasilva@gmail.com>
2021-11-25 23:46:01 -0300

    🎸 Atualizando a configuração da notificação

--- public/js/client.js
+++ public/js/client.js
@@ -1,18 +1,20 @@
 // settings for the client
-const TrelloInframe = window.TrelloPowerUp.iframe()
 const grayIcon = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg'
 
 function generateBranchName() {
-  const hash = window.location.pathname.match(/\/c\/(.*?)\//)[0].replace(/\/c\//g, '').replace(/\//g, '')
+  const hash = window.location.pathname
+    .match(/\/c\/(.*?)\//)[0]
+    .replace(/\/c\//g, '')
+    .replace(/\//g, '')
   const branchName = `card/${hash}`
   return branchName
 }
 
-async function copyInToClipboard(text) {
+async function copyInToClipboard(text, instance) {
   try {
     await window.navigator.clipboard.writeText(text)
   } catch (err) {
-    TrelloInframe.alert(`Error: ${err}`)
+    instance.alert(`Error: ${err}`)
   }
 }
 
@@ -24,7 +26,7 @@ window.TrelloPowerUp.initialize({
         text: 'Copy branch name',
         callback: function(t, options) {
           const branchName = generateBranchName()
-          copyInToClipboard(branchName)
+          copyInToClipboard(branchName, t)
 
           t.alert('Copied!', 'Branch name copied to clipboard.')
         },
