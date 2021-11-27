0a6d99b37ac7e59f9ff80e47c476ae3576f4b2bd
Alexbrazdasilva <alexbrazdasilva@gmail.com>
2021-11-27 11:49:16 -0300

    🚀

--- public/js/git-helpers.js
+++ public/js/git-helpers.js
@@ -1,14 +1,12 @@
 const clipboard = new ClipboardJS('.btn-d-block-1456')
-const Promises = window.TrelloPowerUp.Promise
 var t = window.TrelloPowerUp.iframe();
 
 
-Promises.All([
-  t.get('card', 'shared', 'trello-id'),
-])
-.spread((trelloId) => {
-  console.log('trello-id', trelloId)
+t.getAll()
+.then(function (data) {
+  console.log(JSON.stringify(data, null, 2));
 })
-(() => {
-  document.querySelector('#actions-github-1456').value = 'Alguma coisa'
-})()
+
+// (() => {
+//   document.querySelector('#actions-github-1456').value = 'Alguma coisa'
+// })()
