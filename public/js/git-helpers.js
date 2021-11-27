719c76313102f60797fa6e263dc054de064c7044
Alexbrazdasilva <alexbrazdasilva@gmail.com>
2021-11-27 12:22:25 -0300

    Realizar promises

--- public/js/git-helpers.js
+++ public/js/git-helpers.js
@@ -1,6 +1,17 @@
 const clipboard = new ClipboardJS('.btn-d-block-1456')
-var t = window.TrelloPowerUp.iframe();
-
+const Promise = window.TrelloPowerUp.Promise
+const t = window.TrelloPowerUp.iframe({
+  appKey: '9f5a91288ed267b276c4e73d145cd326',
+  appName: 'Git Generate Branch Name'
+});
+Promise.all([
+  t.get('card', 'public', 'all'),
+  t.get('board', 'shared', 'all')
+]).then(function (data) {
+  console.log(data)
+}).catch(function (err) {
+  console.error(err)
+})
 t.card('name', 'shared').then(card => {
   console.log(card);
 })
