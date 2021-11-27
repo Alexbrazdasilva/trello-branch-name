bb624b9626e3c8195040ef12dcd4c9439d626b47
Alexbrazdasilva <alexbrazdasilva@gmail.com>
2021-11-27 16:50:22 -0300

    🎸 Adicionar helper com comando git

--- public/js/git-helpers.js
+++ public/js/git-helpers.js
@@ -1,6 +1,7 @@
 const clipboard = new ClipboardJS('.btn-d-block-1456')
 const nameBranch = document.querySelector('#actions-github-1456')
 const commitTitle = document.querySelector('#commit-github-1456')
+const helperBranche = document.querySelector('#helper-branche-1456')
 const Promise = window.TrelloPowerUp.Promise
 
 const t = window.TrelloPowerUp.iframe({
@@ -8,11 +9,16 @@ const t = window.TrelloPowerUp.iframe({
   appName: 'Git Generate Branch Name'
 })
 
-function setBranchName(name = 'empty') {
+function setBranchName(name = '') {
   t.set('card', 'shared', 'branch-name', name)
 }
 
-function setCommitTitle(title = 'empty', branch) {
+function setHelperBranche(branch = '') {
+  const helper = `git checkout -b ${branch}`
+  helperBranche.value = helper
+}
+
+function setCommitTitle(title = '', branch) {
   const titleCommit = title.substring(0, 45)
   const formatedCommitTitle = `${titleCommit} [${branch}]`
   t.set('card', 'shared', 'commit-title', formatedCommitTitle)
@@ -20,7 +26,9 @@ function setCommitTitle(title = 'empty', branch) {
 
 t.card('all')
   .then(({ shortLink, name }) => {
-    setBranchName(`fix/${shortLink}`)
+    const branchName = `fix/${shortLink}`
+    setBranchName(branchName)
+    setHelperBranche(branchName)
     setCommitTitle(name, shortLink)
   })
 
